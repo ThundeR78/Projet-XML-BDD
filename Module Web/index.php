fe1be423977d4d6a8cb101b8b01f1a148588d561
julienZ <jzerbit@sophiacom.fr>
2013-11-14 00:09:39 +0100

    add login form

---
+++ Module Web/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<?php
+	if(isset($_POST['connect']))
+	{
+		if(isset($_POST['username']) && isset($_POST['password']))
+		{
+			
+		}
+		else
+			$error = "Tous les champs sont obligatoires";
+	}
+
+?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Projet XML</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/index.css" />
+	</head>  
+	
+	<body>
+		<div id="global">
+			<h1 id="title">Projet XML - Partie WEB</h1>
+			<div id="loginDiv">
+				<form action="" method="post">
+					<input type="text" name="username" placeholder="Login" required /><br /><br />
+					<input type="password" name="password"  placeholder="Password" required /><br /><br />
+					<input type="submit" value="Connexion" name="connect" />
+				</form>
+			</div>
+			<div id="loginError">
+				<?php if(isset($error)) { echo "<p>".$error."</p>"; } ?>
+			</div>
+		</div>
+	</body>
+	
+</html>
